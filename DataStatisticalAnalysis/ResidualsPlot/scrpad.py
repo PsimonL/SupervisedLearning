acb03439b1867eb58ad25a7ee58ec7978bd75c06
Szymon Rogowski <szymon0r0official@gmail.com>
2022-12-04 15:58:06 +0100

    Refactoring

---
+++ DataStatisticalAnalysis/ResidualsPlot/scrpad.py
@@ -0,0 +1,16 @@
+# https://www.geeksforgeeks.org/python-bitwise-operators/
+
+a = 1
+b = 2
+
+if a == 1 and b == 2:
+    print("+")
+else:
+    print("-")
+
+print()
+
+if a == 1 | b == 2:
+    print("+")
+else:
+    print("-")
