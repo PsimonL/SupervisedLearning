a7558544e64987bcb5b40b332c7e42dde7fce88a
Szymon Rogowski <szymon0r0official@gmail.com>
2022-12-05 04:28:11 +0100

    RANSAC for Lin Reg #1

--- RANSAC_for_LinearRegression/scratchpad.py
+++ RANSAC_for_LinearRegression/scratchpad.py
@@ -51,43 +51,43 @@ print(X[idx])
 
 
 
-# From Udemy course. No need to do it myself.
-df = pd.read_csv(r"housing.data",
-                   sep="" +"", engine="python", header=None,
-                   names=["CRIM", "ZN", "INDUS", "CHAS", "NOX", "RM", "AGE", "DIS",
-        "RAD", "TAX", "PTRATIO", "B", "LSTAT", "MEDV"])
-
-X = df.loc[:, "LSTAT"].values.reshape(-1, 1)
-y = df["MEDV"].values
-
-scaler = StandardScaler()
-scaler.fit(X)
-X = scaler.transform(X)
-
-ransac = RANSAC1(max_iters_k=100, treshold=20, min_acceptable_inliers=100)
-
-ransac.fit(X, y, show_partial_results=True)
-
-X_train, X_test, y_train, y_test = train_test_split(X[ransac.inlier_mask],
-                                                    y[ransac.inlier_mask],
-                                                    test_size=0.3)
-
-lr = LinearRegression()
-lr.fit(X_train, y_train)
-y_pred = lr.predict(X_test)
-
-line_X = np.arange(X.min(), X.max())[:, np.newaxis]
-line_y = ransac.best_model[0] * line_X + ransac.best_model[1]
-
-plt.figure(figsize=(7, 5))
-plt.scatter(X[ransac.inlier_mask], y[ransac.inlier_mask], color="green",
-            marker=""."", label="Inliers")
-plt.scatter(X[~ransac.inlier_mask], y[~ransac.inlier_mask], color="red",
-            marker=""."", label="Outliers")
-plt.plot(line_X, line_y, color="blue", linewidth=2, label="RANSAC")
-plt.plot(X_test, y_pred, color="red", label="Linear regression")
-
-plt.legend(loc="lower right")
-plt.xlabel("Input")
-plt.ylabel("Response")
-plt.show()
+# # From course. No need to do it myself.
+# df = pd.read_csv(r"housing.data",
+#                    sep="" +"", engine="python", header=None,
+#                    names=["CRIM", "ZN", "INDUS", "CHAS", "NOX", "RM", "AGE", "DIS",
+#         "RAD", "TAX", "PTRATIO", "B", "LSTAT", "MEDV"])
+#
+# X = df.loc[:, "LSTAT"].values.reshape(-1, 1)
+# y = df["MEDV"].values
+#
+# scaler = StandardScaler()
+# scaler.fit(X)
+# X = scaler.transform(X)
+#
+# ransac = RANSAC1(max_iters_k=100, treshold=20, min_acceptable_inliers=100)
+#
+# ransac.fit(X, y, show_partial_results=True)
+#
+# X_train, X_test, y_train, y_test = train_test_split(X[ransac.inlier_mask],
+#                                                     y[ransac.inlier_mask],
+#                                                     test_size=0.3)
+#
+# lr = LinearRegression()
+# lr.fit(X_train, y_train)
+# y_pred = lr.predict(X_test)
+#
+# line_X = np.arange(X.min(), X.max())[:, np.newaxis]
+# line_y = ransac.best_model[0] * line_X + ransac.best_model[1]
+#
+# plt.figure(figsize=(7, 5))
+# plt.scatter(X[ransac.inlier_mask], y[ransac.inlier_mask], color="green",
+#             marker=""."", label="Inliers")
+# plt.scatter(X[~ransac.inlier_mask], y[~ransac.inlier_mask], color="red",
+#             marker=""."", label="Outliers")
+# plt.plot(line_X, line_y, color="blue", linewidth=2, label="RANSAC")
+# plt.plot(X_test, y_pred, color="red", label="Linear regression")
+#
+# plt.legend(loc="lower right")
+# plt.xlabel("Input")
+# plt.ylabel("Response")
+# plt.show()
